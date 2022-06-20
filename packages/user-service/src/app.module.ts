e6cc7f1cadbb484515eda39194d7f1909a7c318c
Nicolas Fernandes <>
2022-06-20 20:17:49 +0200

    Setting up GraphQL within user-service.

--- packages/user-service/src/app.module.ts
+++ packages/user-service/src/app.module.ts
@@ -1,15 +1,26 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppController } from './app.controller';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 import { CustomMongoDBLogger } from './common/mongoose/custom-mongodb-logger';
 
 @Module({
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URL, {
       logger: CustomMongoDBLogger,
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
