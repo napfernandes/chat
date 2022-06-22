251119db418f0140f8027db6967af96bfe617fe7
Nicolas Fernandes <>
2022-06-22 12:07:22 +0200

    Getting url from NestJS app for conversation-service.

--- packages/conversation-service/src/main.ts
+++ packages/conversation-service/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import 'reflect-metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.SERVER_PORT);
+
+  const appUrl = await app.getUrl();
+
+  console.log(`App started at ${appUrl}.`);
 }
 bootstrap();
