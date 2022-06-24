05e4f4ebe1d78478ae1772dccf09ce2c7b465aa7
Nicolas Fernandes <>
2022-06-24 17:39:53 +0200

    [chat-frontend] Renaming folder.

--- packages/chat-frontend/src/index.tsx
+++ packages/chat-frontend/src/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import LoginView from './views/login/view';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './index.css';
+import HomeView from './views/home/view';
+import LoginView from './views/login/view';
+import reportWebVitals from './reportWebVitals';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <LoginView />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<HomeView />} />
+        <Route path="/login" element={<LoginView />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ packages/chat-frontend/src/views/home/view.tsx
@@ -0,0 +1,5 @@
+export default function HomeView() {
+    return (
+        <h1>HOME</h1>
+    )
+}
