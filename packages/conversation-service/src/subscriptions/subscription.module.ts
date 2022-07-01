34d7f51136ffe997f5ef373e95b5f8eb6ccbcf88
Nicolas Fernandes <>
2022-07-01 09:52:50 +0200

    [conversation-service] Removing reacting to ConversationStarted from this service for now.

--- packages/conversation-service/src/subscriptions/subscription.module.ts
+++ packages/conversation-service/src/subscriptions/subscription.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
 
 import { QueueModule } from '@napfernandes/queue';
-import { PubsubModule, SubscriptionService } from '@napfernandes/pubsub';
-import { ConversationStartedSubscription } from './conversation-started.subscription';
+import { SubscriptionService } from '@napfernandes/pubsub';
 
 @Module({
-  imports: [PubsubModule, QueueModule],
-  providers: [SubscriptionService, ConversationStartedSubscription],
+  imports: [QueueModule],
+  providers: [SubscriptionService],
 })
 export class SubscriptionModule {
-  constructor(
-    private readonly subscriptionService: SubscriptionService,
-    private readonly conversationStartedSubscription: ConversationStartedSubscription,
-  ) {
-    this.subscriptionService.on(this.conversationStartedSubscription);
-  }
+  constructor(private readonly subscriptionService: SubscriptionService) {}
 }
